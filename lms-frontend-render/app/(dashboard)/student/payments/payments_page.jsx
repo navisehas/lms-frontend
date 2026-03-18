35f897a4b4e8eaedc5218c89959669acd3780137
Naviru Sehas <navirusehas27@gmail.com>
2026-03-18 12:59:20 +0530

    updated

--- lms-frontend-render/app/(dashboard)/student/payments/payments_page.jsx
+++ lms-frontend-render/app/(dashboard)/student/payments/payments_page.jsx
@@ -95,6 +95,8 @@ export default function StudentCoursesPayPage() {
         country:       "Sri Lanka",
         hash:          hashData.hash,
         platform:      "web",
+        custom_logo:   `${FRONTEND_URL}/english-gate-logo.jpeg`,
+        merchant_name: "English Gate",
       };
 
       Object.entries(fields).forEach(([k, v]) => {
